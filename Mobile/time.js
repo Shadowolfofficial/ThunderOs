e633293f388471a5cb4f097613913e262738855a
ThunderOs <107752514+Shadowolfofficial@users.noreply.github.com>
2023-05-24 19:15:37 +0200

    Update time.js

--- Mobile/time.js
+++ Mobile/time.js
@@ -1,24 +1,20 @@
-$(document).ready(function () {
-    setTime();
+$(document).ready(function() {
+  setInterval(setTime, 1000);
 });
 
-function setTime(){
-    var dayList=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    var monthList=["January","February","March","April","May","June","July","August","September","October","November","December"];
-    var now = new Date();
-    var hour = now.getHours();
-    if(hour>12){
-        hour-=12;
-    }
-    else if(hour==0){
-        hour=12;
-    }
-    time=hour+":"+checkZero(now.getMinutes());
-    date=dayList[now.getDay()]+", "+monthList[now.getMonth()]+" "+now.getDate();
-    $(".bottom .container .calender p").text(dayList[now.getDay()]);
-    $(".bottom .container .calender h1").text(now.getDate());
+function setTime() {
+  const dayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const now = new Date();
+  const hour = now.getHours();
+  const time = `${checkZero(hour)}:${checkZero(now.getMinutes())}`;
+  const date = `${dayList[now.getDay()]}, ${monthList[now.getMonth()]} ${now.getDate()}`;
+  $(".bottom .container .calender p").text(dayList[now.getDay()]);
+  $(".bottom .container .calender h1").text(now.getDate());
+  $(".main .left .time h1").text(time);
+  $(".main .left .time p").text(date);
+}
 
-    $(".main .left .time h1").text(time);
-    $(".main .left .time p").text(date);
-    setTimeout(setTime,1000);
+function checkZero(i) {
+  return i < 10 ? `0${i}` : i;
 }

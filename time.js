35d96c3c880e0dcd1007066259b2210aada70dd9
ThunderOs <107752514+Shadowolfofficial@users.noreply.github.com>
2023-05-24 18:30:11 +0200

    Update time.js

--- time.js
+++ time.js
@@ -7,12 +7,12 @@ function setTime() {
   const monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   const now = new Date();
   const hour = now.getHours();
-  const time = `${hour % 12}:${checkZero(now.getMinutes())} ${hour >= 24 ? 'PM' : 'AM'}`;
+  const time = `${hour}:${checkZero(now.getMinutes())}`;
   const date = `${dayList[now.getDay()]}, ${monthList[now.getMonth()]} ${now.getDate()}`;
   $(".bottom .container .calender p").text(dayList[now.getDay()]);
   $(".bottom .container .calender h1").text(now.getDate());
   $(".main .left .time h1").text(time);
-  $(".main .left .time p").text(`${now.toLocaleDateString()} ${now.toLocaleTimeString()}`);
+  $(".main .left .time p").text(`${now.toLocaleDateString()} ${now.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', hour12: false})}`);
 }
 
 function checkZero(i) {

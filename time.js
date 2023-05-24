a64025f9e82ff7da6ec24d27c1bc80b56816996c
ThunderOs <107752514+Shadowolfofficial@users.noreply.github.com>
2023-05-24 18:28:42 +0200

    Update time.js

--- time.js
+++ time.js
@@ -7,7 +7,7 @@ function setTime() {
   const monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   const now = new Date();
   const hour = now.getHours();
-  const time = `${hour % 12}:${checkZero(now.getMinutes())} ${hour >= 12 ? 'PM' : 'AM'}`;
+  const time = `${hour % 12}:${checkZero(now.getMinutes())} ${hour >= 24 ? 'PM' : 'AM'}`;
   const date = `${dayList[now.getDay()]}, ${monthList[now.getMonth()]} ${now.getDate()}`;
   $(".bottom .container .calender p").text(dayList[now.getDay()]);
   $(".bottom .container .calender h1").text(now.getDate());
